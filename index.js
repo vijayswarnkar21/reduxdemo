39e3648eaf59996749ebe20e416c265addc1d44b
vijay kumar swarnkar <vijay.kumarswaranakar@girnarsoft.co.in>
2020-11-26 13:38:34 +0530

    single reduces/shopkeeper for multiple items

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 //Here this function is called action creater as it is returning an action
 //action is a object with a propery named as 'type'
@@ -17,11 +18,19 @@ const buyCake = () => {
     } 
 }
 
+const buyIceCream = () => {
+    return {
+        type:BUY_ICECREAM,
+        info: "First Redux Action"
+    } 
+}
+
 //reducer
 //(previousState,action) => newState
 
 const intialState = {
-    numOfCakes : 10
+    numOfCakes : 10,
+    numOfIceCream: 20
 }
 
 //Here we don mutate the state object we return new object
@@ -32,6 +41,13 @@ const reducer = (state = intialState, action) => {
             //then change only the properties that need to be changed
             ...state,    
             numOfCakes : state.numOfCakes -1
+        }
+
+        case BUY_ICECREAM : return {
+            //it is safe to first create the copy of state object and 
+            //then change only the properties that need to be changed
+            ...state,    
+            numOfIceCream : state.numOfIceCream -1
         }
 
         default : return state
@@ -59,6 +75,8 @@ store.dispatch(buyCake());
 store.dispatch(buyCake());
 //customer 3
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 
 
 unSubscribe();
